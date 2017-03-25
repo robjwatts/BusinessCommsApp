5feebd4964eb10081acbe523dc72c936106fb086
matt <matthewjkim@gmail.com>
2017-03-25 01:03:47 -0400

    cleaning up some files

--- apis/gsuite-service-account.js
+++ apis/gsuite-service-account.js
@@ -71,11 +71,6 @@ function createJWT() {
 	var privateKey = fs.readFileSync(path.join(__dirname, 'server.key'));
 
 
-	//get our private key to sign our JWT
-	var secret = process.env.PRIVATE_KEY;
-	console.log('secret', secret);
-	console.log('payload', payload);
-
 	//sign() is jsonwebtoken function to encrypt to base64 header, claim set and secret
 	var jsonWebToken = jwt.sign(payload, privateKey, { algorithm: 'RS256', expiresIn: '1h' });
 
